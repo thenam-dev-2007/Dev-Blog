22e7f0d46387c345c1869b8eff03cab639851042
Vu Duy Anh <AnhVD.B25CC026@stu.ptit.edu.vn>
2026-05-15 23:25:15 +0700

    back

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -2,16 +2,23 @@ const express = require("express");
 require("dotenv").config();
 
 const database = require("./config/database");
-const routeClient = require("./routes/client/index.route") 
+const routeClient = require("./routes/client/index.route");
+const routeAdmin = require("./routes/admin/index.route");
 
 const app = express();
 const port = process.env.PORT;
 
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 // Kết nối database
 database.connect();
 
+// Routes
 routeClient(app);
+app.use("/admin", routeAdmin);
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
-})
+});
