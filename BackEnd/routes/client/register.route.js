fd715d1c742415f7b5ba2a7f3b8fd818f506c2e7
Vu Duy Anh <AnhVD.B25CC026@stu.ptit.edu.vn>
2026-05-19 21:31:21 +0700

    Back 05/19

--- BackEnd/routes/client/register.route.js
+++ BackEnd/routes/client/register.route.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
-const controller = require("../../controllers/client/user.controller");
+const controller = require("../../controllers/client/register.controller");
 const validation = require("../../middlewares/validation");
 
-router.post("/", validation.validateCreateUser, controller.createUser); // Dấu : dùng để truyền data động
+router.post("/", validation.validateCreateUser, controller.register); // Dấu : dùng để truyền data động
+router.post("/check-username", controller.checkUsername);
+router.post("/check-email", controller.checkEmail);
+router.get("/inactive", controller.getInactiveUsers);
+router.post("/:id/restore", controller.restoreAccount);
 
 module.exports = router;
