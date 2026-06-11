d535e0a5406cfd50363b768b7b6be37ff13ec9f8
Nguyen The Nam <namnt2007.ptit@gmail.com>
2026-06-11 21:15:04 +0700

    update

--- BackEnd/routes/admin/index.route.js
+++ BackEnd/routes/admin/index.route.js
@@ -1,6 +1,16 @@
 const systemConfig = require("../../config/system.js");
+const authRoute = require("../../routes/admin/auth.route");
+const profileRoute = require("../../routes/admin/auth.route");
+const postRoute = require("../../routes/admin/post.route");
+
 
 module.exports = (app) => {
     const PATH_API = systemConfig.prefixApi;
     const PATH_ADMIN = systemConfig.prefixAdmin;
+
+    app.use(PATH_API + "/auth", authRoute);
+
+    app.use(PATH_API + "/posts", postRoute);
+
+    app.use(PATH_API + "/profile", profileRoute);
 };
---
+++ BackEnd/routes/admin/post.route.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+
+const controller = require("../../controllers/admin/post.controller.js");
+const validation = require("../../middlewares/validation.js");
+const upload = require("../../middlewares/upload.js");
+const auth = require("../../middlewares/auth.js");
+const postMiddleware = require("../../middlewares/post.middleware.js");
+
+router.get("/", controller.getAllPosts);
+
+router.get("/:slug", controller.getPostBySlug);
+
+router.post("/", auth.authenticateToken, upload.uploadThumbnail.single("thumbnail"), validation.validateCreatePost, controller.createPost);
+
+router.delete("/:id", auth.authenticateToken, postMiddleware.loadPost, postMiddleware.canDeletePost, controller.deletePost);
